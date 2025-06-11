34af8b3a7a04eccc59fbf8259226cb352d4f67a7
JairoHervert <158542532+JairoHervert@users.noreply.github.com>
2025-06-11 08:40:29 -0600

    chat/ added authmiddleware to messages routes

--- src/presentation/messages/messages.routes.ts
+++ src/presentation/messages/messages.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { MessagesChatController } from './message.controller';
+import { AuthMiddleware } from '../middleware/prestamista.auth.middleware';
 
 export class MessagesChatRoutes {
   static get routes(): Router {
     const router = Router();
     const controller = new MessagesChatController();
     
-    router.post('/send', controller.sendMessage);
-    router.get('/last-messages/:sender/', controller.obtainLastUserChats);
-    router.get('/messages/:sender/:receiver', controller.obtainOneChat);
+    router.post('/send', [AuthMiddleware.validateJWT] ,controller.sendMessage);
+    router.get('/last-messages/:sender/', [AuthMiddleware.validateJWT] , controller.obtainLastUserChats);
+    router.get('/messages/:sender/:receiver', [AuthMiddleware.validateJWT] , controller.obtainOneChat);
     
     return router;
   }
