51c4f10c5dd56270febd826763af5bcb2debe615
dngxue <paolareyes802@gmail.com>
2025-06-27 02:10:57 -0600

    Endpoint para obtener los datos del perfil del cliente

--- src/domain/repositories/client.repository.ts
+++ src/domain/repositories/client.repository.ts
@@ -6,4 +6,5 @@ export interface ClientRepository {
   create(data: CreateClientDto): Promise<ClientEntity>;
   findByCorreo(correo: string): Promise<ClientEntity | null>;
   completeData(idCliente: number, data: CompleteDataClientDto): Promise<ClientEntity | null>;
+  getDataById(id: number): Promise<ClientEntity | null>;
 }
